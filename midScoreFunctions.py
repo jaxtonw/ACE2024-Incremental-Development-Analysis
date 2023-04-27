7086ecb537b98f2e250582a5a74c80e4f13eebc8
Jaxton Winder <jjwinder9@gmail.com>
2023-04-26 19:50:32 -0600

    Filtered out files from midscore calculation that are not source code or autogenerated by PyCharm

--- midScoreFunctions.py
+++ midScoreFunctions.py
@@ -82,6 +82,15 @@ def get_mid_score_all(final_data, student_df):
     '''
     mid_score_df = pd.DataFrame()
     for student, assignment, file_states in final_data:
+        new_file_states = {}
+        for file_name in file_states:
+            # Filtering out 'bad' filenames
+            if not file_name.endswith('.py') or '.txt' in file_name.lower() or 'plan' in file_name.lower() or 'main' in file_name.lower():
+                print(f"filtering out file {file_name}")
+                continue
+            new_file_states[file_name] = file_states[file_name]
+        file_states = new_file_states
+
         if len(file_states) == 0:
             continue
         all_file_states = []
